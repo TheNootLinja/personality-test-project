f2e79c8db4eb07fc2397f68e5f8b8bf30fe4f9d7
TheNootLinja <thenootlinja@gmail.com>
2022-09-30 13:58:53 -0400

    Fix git sub repo issue

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx/,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E9C376",
+        secondary: "#DFB8AC"
+      },
+    },
+    fontFamily: {
+      'display': ["Mirage"],
+      'sans': ['"Nunito Sans"'],
+      'handwriting': 'BrittanySignature',
+      'serif': ['"Roboto Slab"'],
+    }
+  },
+  plugins: [],
+}
