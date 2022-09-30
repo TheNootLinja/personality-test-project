225ce4e4e04952cdb2aa32979269c22ed054a604
TheNootLinja <thenootlinja@gmail.com>
2022-09-30 14:25:18 -0400

    Add src folder and move all written code to that location and add component folder

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className="font-display">Hello World</h1>
+      <h1 className="font-handwriting">Hello World</h1>
     </div>
   )
 }
