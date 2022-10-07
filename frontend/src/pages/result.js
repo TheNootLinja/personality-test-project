e51e0ef20ea868601e8e22977ad38fa95b492c6c
TheNootLinja <thenootlinja@gmail.com>
2022-10-07 10:33:30 -0400

    Add result page and styling

--- frontend/src/pages/result.js
+++ frontend/src/pages/result.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Page from '../components/Page'
 import Answers from '../components/Answers'
+import Diagram from '../components/Diagram'
 
 const Question = () => {
   return (
@@ -13,6 +14,9 @@ const Question = () => {
               <div className="font-handwriting text-6xl w-full text-left -mb-4 relative -left-10">Hello</div>
               Three
               </div>
+                <div className='max-w-md mx-auto mb-5'>
+                  <Diagram />
+                </div>
                 <p className="text-left">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus cum voluptatibus a at iure facere illo nihil corporis praesentium, animi debitis aspernatur ipsam commodi nesciunt inventore quibusdam quisquam labore numquam!</p>
             </div>
         </div>
